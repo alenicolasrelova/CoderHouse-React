75d466ab12fdac12434ab4d26a5e0813db1f078f
AlejandroMio <alenicolasrelova@gmail.com>
2022-01-19 22:46:43 -0300

    componentes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import { NavBarrr } from "./components/navBar/navBar";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { ClassComponent } from './components/class-component/ClassComponent';
+import functionalComponente from './components/function-component/functionalComponente';
+
+
+const age = 18;
 function App() {
   return (
     <div className="App">
       <NavBarrr />
-      
+      <ClassComponent age={age} name="alejandro"/>
+      <functionalComponente />
     </div>
   );
 }
---
+++ src/components/function-component/functionalComponente.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const functionalComponente = (name) => {
+    return (
+        <div>
+            <h1>FunctionalComponente</h1>
+            <h2>nombre : {this.props.name}</h2>
+        </div>
+    );
+};
+
+export default functionalComponente;
